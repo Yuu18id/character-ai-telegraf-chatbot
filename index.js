def79cf8a03d34d473d484af5d96d07f091b12c0
Bayu Arma Praja <100572149+Yuu18id@users.noreply.github.com>
2023-02-10 20:07:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Telegraf = require('telegraf');
+const bot = new Telegraf(''); //bot token
+const { read } = require('fs');
+const CharacterAI = require('node_characterai');
+const characterAI = new CharacterAI();
+
+(async() => {
+    await characterAI.authenticateWithToken(''); //account token or using guest authenticateAsGuest() 
+    const characterId = "" //characterid token
+    const chat = await characterAI.createOrContinueChat(characterId);
+    
+    bot.start((ctx) => {
+        ctx.reply('Welcome '+ ctx.from.first_name)
+    });
+
+    bot.on('text', async(ctx) => {
+        const response = await chat.sendAndAwaitResponse(ctx.message.text, true)
+        ctx.reply(response)
+    });
+})();
+
+bot.launch()
